f082308aa187997f690aa5bf4674024eaa0ca697
Iris <ch_yj07@163.com>
2025-02-12 21:46:20 +0800

    feat: init

---
+++ apps/docs/eslint.config.js
@@ -0,0 +1,4 @@
+import { nextJsConfig } from "@turbo-demo/eslint-config/next-js";
+
+/** @type {import("eslint").Linter.Config} */
+export default nextJsConfig;
---
+++ packages/lib/eslint.config.mjs
@@ -0,0 +1,4 @@
+import { config } from "@turbo-demo/eslint-config/react-internal";
+
+/** @type {import("eslint").Linter.Config} */
+export default config;
